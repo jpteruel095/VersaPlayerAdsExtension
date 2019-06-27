994712f136e891f787ea8a16235b7e3b06557c7b
jpteruel095 <jpteruel95@gmail.com>
2019-06-27 13:56:59 +0800

    Removed example project

--- Example/Pods/Target Support Files/Pods-VersaPlayerAdsExtension_Example/Pods-VersaPlayerAdsExtension_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_VersaPlayerAdsExtension_Example : NSObject
-@end
-@implementation PodsDummy_Pods_VersaPlayerAdsExtension_Example
-@end
--- Example/Pods/Target Support Files/VersaPlayerAdsExtension/VersaPlayerAdsExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_VersaPlayerAdsExtension : NSObject
-@end
-@implementation PodsDummy_VersaPlayerAdsExtension
-@end
